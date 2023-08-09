89808f748830f7b8f29dc33a78a0f8f3cf25cf3a
Christopher McFarland <christopher.mcfarland@case.edu>
2023-08-08 22:26:57 -0400

    very close

--- bin/derep.py
+++ bin/derep.py
@@ -6,8 +6,11 @@ from pathlib import Path
 from AmpliconPE import SimplexMasterRead as MasterRead, pairedFASTQiter, get_PE_FASTQs
 from collections import Counter
 
-
-def derep(FASTQ_directory: Path, master_read: str, trim_fraction: float = 0.6):
+def derep(
+    FASTQ_directory: Path,
+    master_read: str,
+    trim_fraction: float = 0.6
+):
     """Lossless barcode extraction from Paired-End (PE) FASTQ reads & dereplicates
 
     Args:
@@ -18,10 +21,11 @@ def derep(FASTQ_directory: Path, master_read: str, trim_fraction: float = 0.6):
     # in case running interactively, convert strings to paths
     FASTQ_directory = Path(FASTQ_directory)
 
-    master_read = MasterRead(master_read, trim_fraction=trim_fraction)
+    master_read = MasterRead(master_read, trim_fraction = trim_fraction)
 
     pileups = Counter()
     scores = Counter()
+
 
     file_pair = get_PE_FASTQs(FASTQ_directory)
     FASTQ_iter = pairedFASTQiter(*file_pair)
@@ -33,22 +37,26 @@ def derep(FASTQ_directory: Path, master_read: str, trim_fraction: float = 0.6):
 
     ## Output
     max_scores = master_read.self_alignment.get_scores()
+    
+    score_ixs = np.fromiter(scores.keys(), dtype=np.dtype((float, len(max_scores))))
+    normalized_scores = score_ixs.div(max_scores, axis=1)
 
-    scores = pd.Series(
-        {
-            tuple((idx / max_i for idx, max_i in zip(idxs, max_scores))): counts
-            for idxs, counts in scores.items()
-        },
-        name="reads",
-    )
+    score_series = pd.Series(scores.values(), 
+        index=pd.MultiIndex.from_arrays(normalized_scores, 
+            names=master_read.self_alignment.align_pairs),
+                name='reads')
 
-    scores.index.names = master_read.self_align.align_pairs
+    score_series.to_csv(FASTQ_directory / "scores.csv")
 
-    scores.to_csv(FASTQ_directory / "scores.csv")
-
-    pileups = pd.Series(pileups, name="reads")
-    pileups.index.names = "target", "barcode"
+    pileups = pd.Series(pileups.values(), name="reads",
+        index = pd.MultiIndex.from_tuples(pileups.keys(), names=['score', 'barcode']))
     pileups.to_csv(FASTQ_directory / "pileups.csv")
+
+
+
+        # Think about Integer vs. Float for scores & pileups
+
+
 
     final_marginal = scores.groupby(level="Core-Ref").sum()
     outcomes = pd.Series(
