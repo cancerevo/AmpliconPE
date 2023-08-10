30126e427a442d85f10017fa2b8082c36414dcd7
Christopher McFarland <christopher.mcfarland@case.edu>
2023-08-10 18:22:23 -0400

    there

--- bin/report.py
+++ bin/report.py
@@ -25,6 +25,8 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
+threshold = 0.5
+
 info = pd.read_csv("consolidated_info.csv", index_col=["alignment", "sample"])[
     "max_value"
 ]
@@ -42,29 +44,83 @@ scores = pd.read_csv(
 )["reads"]
 
 
+pileups = pd.read_csv("consolidated_pileups.csv", index_col=['sample', 'barcode'])
+
+pileups_gb = pileups.groupby(level=[0,1])
+
+pileups_marginal = pd.DataFrame({
+    'mean alignment' : pileups_gb.agg(lambda df: df.eval('score * reads').sum()),
+    'reads': pileups_gb['reads'].sum()})
+
+pileups_marginal['mean_alignment'] /= pileups_marginal['reads'] # Normalize mean
+
 base_scores = pd.DataFrame(
     {name: scores.groupby(level=name).sum() for name in align_pairs}
 )
 normed_scores = base_scores.div(max_scores, axis=1)
+
+
+normed_scores['pileups'] = np.histogram(
+    pileups_marginal['mean_alignment']
+    weights=pileups_marginal['reads'],
+    bins=len(normed_scores),
+    range=(0,1)
+    )[0]
+
 print(normed_scores.describe())
 base_scores.columns.name = "alignment"
 longform = base_scores.stack()
 longform.name = "reads"
 
-print(base_scores)
-
 
 sns.ecdfplot(
     data=longform.reset_index(),
     x="score",
-    weights="reads",
+    weights=longform.name,
     hue="alignment",
     stat="count",
-    hue_order=align_pairs,
+    hue_order=normed_scores.columns,
     ax=plt.gca(),
 )
 plt.savefig("alignments.pdf")
 
+def length(bc):
+    return pd.Series(bc).str.len().std()
 
-ref_scores = scores.groupby(level=["Fwd-Ref", "Rev-Ref"]).sum()
-composite_scores = ref_scores.groupby(lambda ix: ix[0] + ix[1]).sum()
+from scipy.stats import entropy
+p_stats = pileups.groupby(level='score').agg(dict(barcode=length_stdev, reads=entropy)
+p_stats['Fwd-Rev Alignment'] = scores.groupby('Core-Ref')['Fwd-Rev'].mean() / max_scores['Fwd-Rev']
+p_stats.columns.name = 'stat'
+p_stats /= p.stats.max()/100
+
+line_data = p_stats.stack()
+line_data.name = '% of Max'
+
+
+
+sns.lineplot(
+    x='score',
+    y=line_data.name,
+    hue='stat'
+
+
+[ pileups_gb.groups.keys()]
+
+# What we want:
+#   - self-similarity declines with alignment quality
+#   - Lengths diversify
+#   - barcodes reduce in diversity
+#
+#
+
+print('f {:}'
+
+hue_order = 
+sns.barplot(
+    x='hample',
+    y'=reads',
+    hue='total'
+
+
+
+
