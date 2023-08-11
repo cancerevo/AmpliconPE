67d566f938e95e28ddf3c90a16737fe11607babd
Christopher McFarland <christopher.mcfarland@case.edu>
2023-08-10 23:37:25 -0400

    done

--- bin/report.py
+++ bin/report.py
@@ -11,7 +11,7 @@ Questions-Evidence (E) pairs:
     E: Flat Cap -> wrong reference
 
 3. Did any samples have poor PCR?
-    E: Fwd-Rev behind Fwd/Ref - Ref
+    E: fwd-rev behind fwd/ref - ref
 
 4. Did any samples have poor index matching?
     E: Index mismatch %
@@ -34,42 +34,36 @@ index_mismatches = info.pop("Index Mismatches")
 
 gb = info.groupby(level="sample")
 max_scores = gb.median().astype(int)
+max_final_score = max_scores['core-ref']
 if not np.allclose(gb.std(), np.zeros(len(gb))):
     raise ValueError("Samples have different maximum alignment values.")
 
 align_pairs = max_scores.index.values
 
+
+pileups = pd.read_csv("consolidated_pileups.csv", index_col=['sample', 'barcode']).eval(
+    'total alignment = score*read/@max_final_score')
+
+pileups_marginal = pileups.groupby(level=[0,1])['total alignment', 'reads']).sum()
+pileups_marginal['mean alignment'] = pileups_marginal.pop('total alignment') / pileups_marginal['reads'] 
+
+
 scores = pd.read_csv(
     "consolidated_scores.csv", index_col=["sample"] + align_pairs, dtype=np.uint64
 )["reads"]
-
-
-pileups = pd.read_csv("consolidated_pileups.csv", index_col=['sample', 'barcode'])
-
-pileups_gb = pileups.groupby(level=[0,1])
-
-pileups_marginal = pd.DataFrame({
-    'mean alignment' : pileups_gb.agg(lambda df: df.eval('score * reads').sum()),
-    'reads': pileups_gb['reads'].sum()})
-
-pileups_marginal['mean_alignment'] /= pileups_marginal['reads'] # Normalize mean
-
 base_scores = pd.DataFrame(
     {name: scores.groupby(level=name).sum() for name in align_pairs}
 )
 normed_scores = base_scores.div(max_scores, axis=1)
-
-
 normed_scores['pileups'] = np.histogram(
-    pileups_marginal['mean_alignment']
+    pileups_marginal['mean alignment']
     weights=pileups_marginal['reads'],
     bins=len(normed_scores),
     range=(0,1)
     )[0]
-
 print(normed_scores.describe())
-base_scores.columns.name = "alignment"
-longform = base_scores.stack()
+normed_scores.columns.name = "alignment"
+longform = normed_scores.stack()
 longform.name = "reads"
 
 
@@ -84,43 +78,53 @@ sns.ecdfplot(
 )
 plt.savefig("alignments.pdf")
 
-def length(bc):
+def length_std(bc):
     return pd.Series(bc).str.len().std()
 
 from scipy.stats import entropy
-p_stats = pileups.groupby(level='score').agg(dict(barcode=length_stdev, reads=entropy)
-p_stats['Fwd-Rev Alignment'] = scores.groupby('Core-Ref')['Fwd-Rev'].mean() / max_scores['Fwd-Rev']
-p_stats.columns.name = 'stat'
-p_stats /= p.stats.max()/100
 
-line_data = p_stats.stack()
+pile_stats = pile_gb_score.agg(dict(barcode=length_stdev, reads=entropy)
+pile_stats['fwd-rev alignment'] = scores.groupby('core-ref')['fwd-rev'].mean() / max_scores['fwd-rev']
+pile_stats.columns.name = 'stat'
+pile_stats /= p.stats.max()/100
+
+line_data = pile_stats.stack()
 line_data.name = '% of Max'
 
-
-
+ax = plt.gca()
 sns.lineplot(
     x='score',
     y=line_data.name,
-    hue='stat'
+    hue='stat',
+    ax=ax)
+
+ax.vline(threshold, ls=':')
+plt.savefig('alignment_profile.pdf')
+
+unaligned = pileups.query('score <= @threshold').groupby(['sample', 'barcode'])['total alignment', 'reads'].sum()
+unaligned['mean alignment'] = unaligned.pop('total alignment') / unaligned['reads']
+unaligned.sort_values(by='reads', ascending=False).to_csv('unaligned.csv')
+
+aligned = pileups.query('score > @threshold').groupby(['sample', 'barcode']['total alignment', 'reads'].sum()
+aligned['mean alignment'] = aligned.pop('total alignment') / aligned['reads']
+aligned.to_csv('aligned_pileups.csv')
+
+outcomes = ['Adapter Index Mismatches', 'Unaligned', 'Aligned']
+sample_stats = pd.concat([
+    index_mismatches, 
+    unaligned.groupy(level='sample')['reads'].sum(),
+    aligned.groupby(level='sample')['reads'].sum()],
+    ,keys=outcomes , names=['outcome'])
 
 
-[ pileups_gb.groups.keys()]
-
-# What we want:
-#   - self-similarity declines with alignment quality
-#   - Lengths diversify
-#   - barcodes reduce in diversity
-#
-#
-
-print('f {:}'
-
-hue_order = 
 sns.barplot(
-    x='hample',
-    y'=reads',
-    hue='total'
+    data=sample_stats,
+    x='sample',
+    y='reads',
+    hue='outcome',
+    hue_order=outcomes,
+    ax=plt.gca())
 
-
+plt.savefig('outcomes.pdf')
 
 
