09dc80aaa5d7de91418552ccf031a491e9765cd2
Christopher D McFarland <christopherdmcfarland@gmail.com>
2023-08-27 20:02:45 -0400

    Create recurrent_read_error.py

---
+++ src/AmpliconPE/recurrent_read_error.py
@@ -0,0 +1,17 @@
+
+
+
+seed_error_model = error_model_inverted_InDels.groupby('From').sum()
+
+def spawn_reads(barcode):
+    neighbors = barcodes.intersection(list(mismatcher(mismatches=1, InDels=True)))
+    barcode_content = content[barcode]
+    delta_content = content[neighbors] - barcode_content
+    return (error_model[delta_content]*aligned.loc[neighbors, 'reads']).sum()
+    
+
+corrected_barcodes = aligned.assign({
+    'Spawn Reads': lambda df: spawn_reads(df['barcode']),
+    'Seed Reads': lambda df: (seed_error_model*content).sum()*df['reads']})
+                                    
+corrected_barcodes.to_csv('error_corrected.csv')
